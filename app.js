e08a9d211ac53adaef2c4b3516ce6b93fb713e14
Alisha M Irfan <160208713+Alisha000111@users.noreply.github.com>
2024-03-17 20:06:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var laptops = [
+    { make: "HP", model: "XPS 19", year: 2024 },
+    { make: "Apple", model: "MacBook Pro", year: 2021 },
+    { make: "Dell", model: "Spectre x460", year: 2022 }
+];
+var laptop1 = laptops[0], laptop2 = laptops[1];
+console.log(laptop1);
+console.log(laptop2);
---
+++ app.ts
@@ -0,0 +1,8 @@
+let laptops = [
+    { make: "HP", model: "XPS 19", year: 2024 },
+    { make: "Apple", model: "MacBook Pro", year: 2021 },
+    { make: "Dell", model: "Spectre x460", year: 2022 }
+];
+let [laptop1, laptop2] = laptops;
+console.log(laptop1);
+console.log(laptop2);
